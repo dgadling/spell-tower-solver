8c6b38eeabf7b7a20a2e1a2e3392cc07677403cf
David Gadling <dave@toasterwaffles.com>
2024-08-18 15:33:32 -0400

    Dictionary now does the things I want, I think

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -5,13 +5,6 @@ use std::path::Path;
 
 use rusqlite::{Connection, Statement};
 
-#[derive(Debug)]
-struct Word {
-    id: u32,
-    word: String,
-    base_points: u8,
-}
-
 pub struct Dictionary {
     source_file: String,
     words_db_path: String,
@@ -107,5 +100,40 @@ impl Dictionary {
             .execute("CREATE INDEX words_word ON words(word)", ())
             .unwrap_or_else(|e| panic!("Failed to create index: {}", e));
         println!("Done init-ing!");
+    }
+
+    fn get_query_for(&mut self, options_len: usize) -> Statement {
+        let placeholders = std::iter::repeat("?")
+            .take(options_len)
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        self.conn
+            .prepare(&format!(
+                "SELECT DISTINCT substr(word, ?, 1) FROM words WHERE substr(word, 1, ?) IN ({})",
+                placeholders
+            ))
+            .expect("Couldn't prepare a statement?!")
+    }
+
+    pub fn get_candidates_for(&mut self, prefix: &str, options: &Vec<&str>) -> Vec<String> {
+        let mut stmt = self.get_query_for(options.len());
+
+        stmt.raw_bind_parameter(1, prefix.len() + 1).unwrap();
+        stmt.raw_bind_parameter(2, prefix.len() + 1).unwrap();
+
+        for option_idx in 0..options.len() {
+            let to_bind = prefix.to_owned() + options.get(option_idx).unwrap();
+            stmt.raw_bind_parameter(option_idx + 3, to_bind).unwrap();
+        }
+
+        let mut rows = stmt.raw_query();
+        let mut candidates = Vec::new();
+        while let Some(row) = rows.next().unwrap() {
+            let foo: String = row.get_unwrap(0);
+            candidates.push(foo);
+        }
+
+        return candidates;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ mod dictionary;
 use dictionary::Dictionary;
 
 fn main() {
-    let dict = Dictionary::new("words_alpha.txt", "dictionary.db", 3);
+    let mut dict = Dictionary::new("words_alpha.txt", "dictionary.db", 3);
+
+    let mut foo = dict.get_candidates_for("pin", &vec!["c", "f", "u", "s", "n", "e"]);
+    println!("Candidates for pin = {:?}", &foo);
+    foo = dict.get_candidates_for("pinu", &vec!["g", "f", "m", "s"]);
+    println!("Candidates for pinu = {:?}", &foo);
+    foo = dict.get_candidates_for("pinus", &vec!["f", "e", "n"]);
+    println!("Candidates for pinus = {:?}", &foo);
 }
