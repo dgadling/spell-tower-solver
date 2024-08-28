559469810ad7d559469b6c1d37dafc90ac1447ca
Dave Gadling <dave@toasterwaffles.com>
2024-08-28 16:53:29 -0400

    [dict][game] Just r2d2
    Single threaded.
    Single `Dictionary` with it's one connection.
    Finished first 3 lines in 28s, 295k boards, ending @ ~ 10k/sec

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use dictionary::Dictionary;
 
 fn main() {
     let dictionary_db_name = "dictionary.db";
-    let mut dict = Dictionary::new(&dictionary_db_name);
-    dict.init_from("nwl/nwl2020.txt", 3);
+    //let mut dict = Dictionary::new(&dictionary_db_name);
+    //dict.init_from("nwl/nwl2020.txt", 3);
 
     let sample_board = vec![
         "i.ssbtpod".chars().map(|c| c.to_string()).collect(),
@@ -16,16 +16,16 @@ fn main() {
         "ln.sgsnnr".chars().map(|c| c.to_string()).collect(),
         "eiusyijme".chars().map(|c| c.to_string()).collect(),
         "olmgapelf".chars().map(|c| c.to_string()).collect(),
-        "tsaeeudhn".chars().map(|c| c.to_string()).collect(),
-        "bsoenditr".chars().map(|c| c.to_string()).collect(),
-        "cwoopteaf".chars().map(|c| c.to_string()).collect(),
-        "itzoutner".chars().map(|c| c.to_string()).collect(),
-        ".upriigal".chars().map(|c| c.to_string()).collect(),
-        "tkayee.ld".chars().map(|c| c.to_string()).collect(),
-        "xlihcrras".chars().map(|c| c.to_string()).collect(),
+        // "tsaeeudhn".chars().map(|c| c.to_string()).collect(),
+        // "bsoenditr".chars().map(|c| c.to_string()).collect(),
+        // "cwoopteaf".chars().map(|c| c.to_string()).collect(),
+        // "itzoutner".chars().map(|c| c.to_string()).collect(),
+        // ".upriigal".chars().map(|c| c.to_string()).collect(),
+        // "tkayee.ld".chars().map(|c| c.to_string()).collect(),
+        // "xlihcrras".chars().map(|c| c.to_string()).collect(),
     ];
 
     let mult_locs: Vec<(usize, usize)> = vec![(0, 8), (1, 2), (9, 6)];
 
-    game::play_game(&mut dict, sample_board, mult_locs);
+    game::play_game(dictionary_db_name, sample_board, mult_locs);
 }
