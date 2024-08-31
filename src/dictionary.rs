0ef3fe69cc41c934250430027ec4eff93e55d965
David Gadling <dave@toasterwaffles.com>
2024-08-30 22:01:53 -0400

    [everything] Make Dictionary a cache, no db access in the hot path

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,38 +1,30 @@
-//use indicatif::ProgressBar;
-use r2d2::PooledConnection;
-use r2d2_sqlite::SqliteConnectionManager;
-//use rusqlite::Connection;
-use std::collections::HashMap;
-//use std::fs::File;
-//use std::io::{BufRead, BufReader};
+use indicatif::{ProgressBar, ProgressStyle};
+use rusqlite::Connection;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub struct Dictionary {
-    conn: PooledConnection<SqliteConnectionManager>,
-    word_cache: HashMap<String, bool>,
-    path_cache: HashMap<String, bool>,
-    path_queries: u64,
-    path_hits: u64,
-    path_queries_by_length: HashMap<usize, u64>,
-    word_queries: u64,
-    word_hits: u64,
+    word_cache: HashSet<String>,
+    path_cache: HashSet<String>,
 }
 
 impl Dictionary {
-    /*
     pub fn new(db_path: &str) -> Self {
-        let conn = Connection::open(db_path)
-            .unwrap_or_else(|e| panic!("Couldn't open {}: {}", db_path, e));
+        let mut d = Dictionary {
+            word_cache: HashSet::new(),
+            path_cache: HashSet::new(),
+        };
 
-        Dictionary {
-            conn,
-            word_cache: HashMap::new(),
-            path_cache: HashMap::new(),
-        }
+        d.prime_caches(db_path);
+        d
     }
 
-    pub fn init_from(&self, source_file: &str, min_word_len: usize) {
+    pub fn init_from(db_path: &str, source_file: &str, min_word_len: usize) {
         println!("Checking database integrity");
-        let rows_res = self.conn.query_row("SELECT count(*) FROM words", [], |r| {
+        let conn = Connection::open(db_path)
+            .unwrap_or_else(|e| panic!("Couldn't open {}: {}", db_path, e));
+        let rows_res = conn.query_row("SELECT count(*) FROM words", [], |r| {
             r.get::<usize, usize>(0)
         });
 
@@ -48,35 +40,21 @@ impl Dictionary {
         };
 
         println!("That doesn't look right. Trying to load data.");
-        self.init_db(source_file, min_word_len);
-    }
-    */
-
-    pub fn with_conn(conn: PooledConnection<SqliteConnectionManager>) -> Self {
-        Dictionary {
-            conn,
-            word_cache: HashMap::new(),
-            path_cache: HashMap::new(),
-            path_queries: 0,
-            path_hits: 0,
-            path_queries_by_length: HashMap::new(),
-            word_queries: 0,
-            word_hits: 0,
-        }
+        Dictionary::init_db(db_path, source_file, min_word_len);
     }
 
-    /*
-    fn init_db(&self, source_file: &str, min_word_len: usize) {
+    fn init_db(db_path: &str, source_file: &str, min_word_len: usize) {
         println!("Creating database of valid words from {}", source_file);
         let in_lines = BufReader::new(File::open(source_file).expect("Couldn't read?!"))
             .lines()
             .map(|l| l.unwrap());
 
-        let bar = ProgressBar::new(370105);
+        let conn = Connection::open(db_path)
+            .unwrap_or_else(|e| panic!("Couldn't open {}: {}", db_path, e));
+        let bar = ProgressBar::new(191745);
 
-        self.conn
-            .execute(
-                "
+        conn.execute(
+            "
                 CREATE TABLE words (
                     id INTEGER PRIMARY KEY,
                     word TEXT NOT NULL,
@@ -84,112 +62,81 @@ impl Dictionary {
                     UNIQUE(word)
                 )
                 ",
-                (),
-            )
-            .unwrap_or_else(|e| panic!("Couldn't create base table: {}", e));
+            (),
+        )
+        .unwrap_or_else(|e| panic!("Couldn't create base table: {}", e));
 
-        self.conn
-            .execute("BEGIN TRANSACTION", ())
+        conn.execute("BEGIN TRANSACTION", ())
             .unwrap_or_else(|e| panic!("Couldn't even start a transaction: {}", e));
         in_lines.filter(|l| l.len() >= min_word_len).for_each(|w| {
-            self.conn
-                .execute(
-                    "INSERT INTO words (word, base_points) VALUES (?1, ?2)",
-                    (&w, 1),
-                )
-                .unwrap_or_else(|e| panic!("Couldn't insert {} into the database: {}", w, e));
+            conn.execute(
+                "INSERT INTO words (word, base_points) VALUES (?1, ?2)",
+                (&w, 1),
+            )
+            .unwrap_or_else(|e| panic!("Couldn't insert {} into the database: {}", w, e));
             bar.inc(1)
         });
         bar.finish();
-        self.conn
-            .execute("COMMIT", ())
+        conn.execute("COMMIT", ())
             .unwrap_or_else(|e| panic!("Couldn't commit the transaction: {}", e));
 
         println!("Optimizing");
-        self.conn
-            .execute_batch(
-                "
+        conn.execute_batch(
+            "
                 CREATE INDEX words_word ON words(word);
                 CREATE VIRTUAL TABLE optimized_words USING FTS5(word);
                 INSERT INTO optimized_words(word) SELECT word FROM words;
                 ",
-            )
-            .unwrap_or_else(|e| panic!("Failed to optimize: {}", e));
+        )
+        .unwrap_or_else(|e| panic!("Failed to optimize: {}", e));
 
         println!("Done init-ing!");
     }
-    */
 
-    pub fn print_stats(&self) {
-        println!(
-            "has_path: queries = {}, hits {}, hit ratio = {}, db queries = {}",
-            self.path_queries,
-            self.path_hits,
-            100.0 * (self.path_hits as f64 / self.path_queries as f64),
-            self.path_queries - self.path_hits
+    fn prime_caches(&mut self, db_path: &str) {
+        println!("Priming caches");
+        let conn = Connection::open(db_path)
+            .unwrap_or_else(|e| panic!("Couldn't open {}: {}", db_path, e));
+
+        let bar = ProgressBar::new(191745);
+        bar.set_style(
+            ProgressStyle::with_template(
+                "{msg} {elapsed} {wide_bar:.blue} {human_pos:>}/{human_len}",
+            )
+            .unwrap()
+            .progress_chars("-> "),
         );
-        println!(
-            " is_word: queries = {}, hits {}, hit ratio = {}, db queries = {}",
-            self.word_queries,
-            self.word_hits,
-            100.0 * (self.word_hits as f64 / self.word_queries as f64),
-            self.word_queries - self.word_hits
-        );
-        println!("path_queries_by_length = {:?}", self.path_queries_by_length);
+
+        bar.set_message("Loading up whole words");
+        let mut stmt = conn.prepare("SELECT word FROM words").unwrap();
+        let mut rows = stmt.query([]).unwrap();
+
+        while let Some(row) = rows.next().unwrap() {
+            let word: String = row.get(0).unwrap();
+            self.word_cache.insert(word);
+            bar.inc(1);
+        }
+        bar.finish();
+
+        bar.set_length(1_706_901);
+        bar.set_message("Loading up paths");
+        for word in self.word_cache.iter() {
+            for l in 2..=word.len() {
+                let mut prefix = String::with_capacity(l);
+                prefix.push_str(&word[0..l]);
+                self.path_cache.insert(prefix);
+                bar.inc(1);
+            }
+        }
     }
 
-    pub fn has_path(&mut self, prefix: &str) -> bool {
-        self.path_queries += 1;
-        self.path_queries_by_length
-            .entry(prefix.len())
-            .and_modify(|c| *c += 1)
-            .or_insert(1);
-        if let Some(ans) = self.path_cache.get(prefix) {
-            self.path_hits += 1;
-            return *ans;
-        }
-
-        if prefix.len() > 15 {
-            // We don't have any words longer than this!
-            self.path_cache.insert(prefix.to_string(), false);
-            return false;
-        }
-        // NOTE: Using the FTS5 table for prefix matching is SEVERAL ORDERS OF MAGNITUDE faster!
-        let query = format!(
-            "SELECT 1 FROM optimized_words WHERE word MATCH '{}*'",
-            prefix
-        );
-
-        let word_found = self
-            .conn
-            .query_row(&query, [], |row| row.get(0) as Result<u32, rusqlite::Error>);
-
-        self.path_cache
-            .insert(prefix.to_string(), word_found.is_ok());
-        word_found.is_ok()
+    pub fn has_path(&self, prefix: &str) -> bool {
+        let has = self.path_cache.get(prefix);
+        has.is_some()
     }
 
-    pub fn is_word(&mut self, prefix: &str) -> bool {
-        self.word_queries += 1;
-        if let Some(ans) = self.word_cache.get(prefix) {
-            self.word_hits += 1;
-            return *ans;
-        }
-
-        if prefix.len() > 15 {
-            // We don't haev any words longer than this!
-            self.path_cache.insert(prefix.to_string(), false);
-            return false;
-        }
-
-        // NOTE: Going the normaly route is SIGNIFICANTLY faster for straight equality checking
-        let query = format!("SELECT word FROM words WHERE word = '{}'", prefix);
-
-        let word = self.conn.query_row(&query, [], |row| {
-            row.get(0) as Result<String, rusqlite::Error>
-        });
-
-        self.word_cache.insert(prefix.to_string(), word.is_ok());
-        word.is_ok()
+    pub fn is_word(&self, prefix: &str) -> bool {
+        let has = self.word_cache.get(prefix);
+        has.is_some()
     }
 }
